fc3ea04afcc82be4b5cdceaa976f6717225a40dd
Yale Alves <yalealves1999@gmail.com>
2023-05-26 17:30:37 -0300

    Adicionado animações na tela com GSAP

--- js/main.js
+++ js/main.js
@@ -8,3 +8,40 @@ playButtonElement.addEventListener('click', function(){
 trailerButtonElement.addEventListener('click', function(){
     window.open('https://www.youtube.com/watch?v=VBdmGK4wxzQ', '_blank');
 })
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    /*--=========== TIMELINE LOADING E HERO ============- */
+  
+    var tl1 = gsap.timeline();
+      
+    tl1
+    .to('.overlay', {
+      duration: 2,
+      opacity: 1, 
+      delay: 1,
+      ease: "power4.out",
+    })
+    .from(".logo-movie", {
+        duration: 2, 
+        opacity: 0, 
+        y: 400,
+        stagger:{each: 0.2},
+      ease: "power4.out",
+    }, '-=1.0')
+    .from("nav", {
+      duration: 2, 
+      opacity: 0, 
+      y: 400,
+      stagger:{each: 0.2},
+      ease: "back.out(1.7)",
+    }, '-=0.5')
+    .from(".details", {
+      duration: 2, 
+      opacity: 0, 
+      y: 200,
+      stagger:{each: 0.2},
+      ease: "back.out(1.7)",
+    }, '-=1.7')
+      
+  });
